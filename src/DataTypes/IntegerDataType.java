15965f8c5ed855655c1107ea6facaf61f0f3ab32
SuhailAkhtar330 <akhtarsuhail330@gmail.com>
2026-03-05 16:26:03 +0530

    Update IntegerDataType.java

--- src/DataTypes/IntegerDataType.java
+++ src/DataTypes/IntegerDataType.java
@@ -2,7 +2,7 @@ package DataTypes;
 
 public class IntegerDataType {
 
-	public static void main({
+	public static void main(String args[]){
 		// TODO Auto-generated method stub
 		
 		// 4. Integer
